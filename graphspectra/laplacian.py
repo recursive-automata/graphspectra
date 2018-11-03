185b06287e35448b822887ba7690bc5f258d95f2
Simon Schneider <simon.r.schneider@gmail.com>
2018-11-03 11:18:31 -0500

    document and clarify

--- graphspectra/laplacian.py
+++ graphspectra/laplacian.py
@@ -1,30 +1,59 @@
 import numpy as np
 
 def compute_in_degrees(adjacency_matrix):
-    """ TODO
+    """Compute the in-degrees of the vertices in a directed graph,
+    the sums of incoming weights in a weighted directed graph,
+    or the degrees of vertices in a simple graph.
+    
+    Args:
+        adjacency_matrix: a numpy 2D array or matrix, representing the
+            adjacency matrix of the graph.
+      
+    Returns: a 1D numpy array
     """
     sum_row = lambda v: v.sum()
     return np.apply_along_axis(sum_row, 0, adjacency_matrix)
 
 
 def compute_out_degrees(adjacency_matrix):
-    """ TODO
+    """Compute the out-degrees of the vertices in a directed graph,
+    the sums of outgoing weights in a weighted directed graph,
+    or the degrees of vertices in a simple graph.
+    
+    Args:
+        adjacency_matrix: a numpy 2D array or matrix, representing the
+            adjacency matrix of the graph.
+      
+    Returns: a 1D numpy array
     """
     sum_col = lambda v: v.sum()
     return np.apply_along_axis(sum_col, 1, adjacency_matrix)
 
 
 def compute_laplacian(adjacency_matrix):
-    """ TODO
+    """Compute the Laplacian matrix of a simple graph.
+    
+    Args:
+        adjacency_matrix: a numpy 2D array or matrix, representing the
+            adjacency matrix of a simple graph.
+      
+    Returns: same type and shape as adjacency_matrix, representing the
+        Laplacian matrix of the graph.
     """
-    # undirected graph => out-degree = in-degree = degree
     degrees = compute_out_degrees(adjacency_matrix)
     deg = np.diag(degrees)
     return deg - adjacency_matrix
 
 
 def compute_normalized_laplacian(adjacency_matrix):
-    """ TODO
+    """Compute the normalized Laplacian matrix of a simple graph.
+    
+    Args:
+        adjacency_matrix: a numpy 2D array or matrix, representing the
+            adjacency matrix of a simple graph.
+      
+    Returns: same type and shape as adjacency_matrix, representing the
+        normalized Laplacian matrix of the graph.
     """
     # undirected graph => out-degree = in-degree = degree
     degrees = compute_out_degrees(adjacency_matrix)
@@ -32,19 +61,3 @@ def compute_normalized_laplacian(adjacency_matrix):
     lap = deg - adjacency_matrix
     deg_sqrt_inv = np.diag(degrees ** -0.5)
     return deg_sqrt_inv * lap * deg_sqrt_inv
-
-
-def compute_directed_laplacian(adjacency_matrix):
-    """ TODO
-    """
-    degrees = compute_out_degrees(adjacency_matrix)
-    length = degrees.shape[0]
-    degrees_tiled = np.tile(np.asmatrix(degrees).transpose(),
-                            [1, length])
-    nonzero_degrees = degrees_tiled != 0
-    egdes = np.divide(adjacency_matrix, degrees_tiled,
-                      out=np.zeros_like(adjacency_matrix),
-                      where=nonzero_degrees)
-    # np.multiply instead of `*` b/c they're matrices
-    diag = np.multiply(nonzero_degrees, np.diag(np.ones(length)))
-    return diag - egdes
