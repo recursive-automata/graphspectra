e922218e2b5924530f2be955da6a2f0e1e1424f6
Simon Schneider <simon.r.schneider@gmail.com>
2018-11-14 15:42:14 -0600

    sparse/dense eigensolvers

--- graphspectra/decompose.py
+++ graphspectra/decompose.py
@@ -1,9 +1,11 @@
 import numpy as np
-from scipy.linalg import eigh
-from scipy.sparse.linalg.eigen.arpack import eigsh
+
+from scipy.sparse import issparse
+from scipy.linalg import eig, eigh
+from scipy.sparse.linalg.eigen.arpack import eigs, eigsh
 
 
-def _reorder_eigensystem(eigensystem, key=None):
+def reorder_eigensystem(eigensystem, key=None):
     key = key or (lambda x: x)
     values, vectors = eigensystem
     indices = np.argsort(key(values))
@@ -12,20 +14,41 @@ def _reorder_eigensystem(eigensystem, key=None):
     return values, vectors
     
 
-def calculate_small_eigens(pos_def_matrix, k=1, dense=False):
-    """ Calculate the smallest eigenvalues and corresponding eigenvectors of
-    a symmetric (or hermitian), positive definite matrix.
+def calculate_small_eigens(hermitian_matrix, k=1):
+    """Calculate the smallest eigenvalues and corresponding eigenvectors of
+    a symmetric (or hermitian) matrix.
     
     Args:
-        pos_def_matrix: a numpy 2D array or matrix, assumed to be positive definite
+        hermitian_matrix: a numpy 2D array or scipy sparse matrix, assumed to be Hermitian.
         k: an integer, how many eigenvalues and eigenvectors to calculate. Default is 1
-        dense: a boolean, whether to use scipy.linalg.eigh to solve eigensystem.
-          If False (default), uses scipy.sparse.linalg.eigen.arpack.eigsh.
       
     Returns: values, vectors. Eigenvalues are in ascending order.
     """
-    if dense:
-        eigensystem = eigh(pos_def_matrix, eigvals=(0, k - 1))
+    sparse = issparse(hermitian_matrix)
+    if sparse:
+        eigensystem = eigsh(hermitian_matrix, k, sigma = 0)
     else:
-        eigensystem = eigsh(pos_def_matrix, k, sigma = 0)
-    return _reorder_eigensystem(eigensystem, key=np.abs)
+        eigensystem = eigh(hermitian_matrix, eigvals=(0, k - 1))
+    return reorder_eigensystem(eigensystem, key=np.abs)
+
+
+def calculate_large_eigens(matrix, k=1):
+    """Calculate the largest-magnitude eigenvalues and corresponding eigenvectors of
+    a matrix.
+    
+    Args:
+        matrix: a numpy 2D array or scipy sparse matrix.
+        k: an integer, how many eigenvalues and eigenvectors to calculate. Default is 1
+      
+    Returns: values, vectors. Eigenvalues are in descending order of magnitude.
+    """
+    sparse = issparse(matrix)
+    key = lambda v: -np.abs(v)  # opposite of magnitude
+    if sparse:
+        eigensystem = eigs(matrix, k=k, which='LM')
+        eigensystem = reorder_eigensystem(eigensystem, key=key)
+        return eigensystem
+    else:
+        eigensystem = eig(matrix)
+        values, vectors = reorder_eigensystem(eigensystem, key=key)
+        return values[0:k], vectors[:, 0:k]
